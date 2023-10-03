6a53fbc3132cffea25d9334ef4eb8f3b2eb0088d
ekapituro <ekapituro@gmail.com>
2023-10-03 13:03:28 +0200

    Update

---
+++ Logger.cs
@@ -0,0 +1,12 @@
+﻿namespace Fupr
+{
+    public class Logger
+    {
+        public void Log(string className, string methodName, string errorMessage)
+        {
+            Console.WriteLine("Class name:" + className);
+            Console.WriteLine("Method name:" + methodName);
+            Console.WriteLine("Error:" + errorMessage);
+        }
+    }
+}
---
+++ StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Fupr
+{
+    public static class StringExtensions
+    {
+        // Extension method by Simon Painter
+        public static string ValueOrDefault(this string? @this, string defaultValue)
+                => string.IsNullOrWhiteSpace(@this) ? defaultValue : @this;
+    }
+}
