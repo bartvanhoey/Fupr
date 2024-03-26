3834d7b097022b7b7b89fd6c27104a314aa3beb6
ekapituro <ekapituro@gmail.com>
2024-03-26 20:13:19 +0100

    Update

--- Extensions/StringExtensions.cs
+++ Extensions/StringExtensions.cs
@@ -9,12 +9,12 @@ namespace Fupr
     {
         // Extension method by Simon Painter
         public static string? ValueOrDefault(this string? @this, string defaultValue)
-                => @this.IsNullOrWhiteSpace() ? defaultValue : @this;
-        
+            => @this.IsNullOrWhiteSpace() ? defaultValue : @this;
+
         // Extension method by Simon Painter
         public static int ValueOrDefault(this string? @this, int defaultValue)
             => @this.IsNullOrWhiteSpace() || !int.TryParse(@this, out var parsedValue) ? defaultValue : parsedValue;
-        
+
         // Modified Extension method by the ABP Framework
         [ContractAnnotation("null => true")]
         public static bool IsNullOrEmpty(this string @this) => string.IsNullOrEmpty(@this);
@@ -26,34 +26,37 @@ namespace Fupr
         // Modified Extension method by the ABP Framework
         [ContractAnnotation("null => true")]
         public static bool IsNullOrWhiteSpace(this string? @this) => string.IsNullOrWhiteSpace(@this);
-        
-        
+
+
         // Modified Extension method by the ABP Framework
         [ContractAnnotation("null => true")]
         public static bool IsNotNullOrWhiteSpace(this string? @this) => !string.IsNullOrWhiteSpace(@this);
-        
+
         // Extension method by Symon Painter
-        public static IEnumerable<IEnumerable<string>> Parser (this string input, string lineSplit, string fieldSplit) =>
+        public static IEnumerable<IEnumerable<string>> Parser(this string input, string lineSplit, string fieldSplit) =>
             input.Split(new[] { lineSplit }, StringSplitOptions.RemoveEmptyEntries)
                 .Select(line => line.Split(new[] { fieldSplit }, StringSplitOptions.RemoveEmptyEntries));
-        
-        
+
+
         // Modified Extension method by the ABP Framework
         [ContractAnnotation("null <= this:null")]
-        public static string ToSentenceCase(this string @this) 
-            => string.IsNullOrWhiteSpace(@this) ? @this : Regex.Replace(@this, "[a-z][A-Z]", m => m.Value[0] + " " + char.ToLowerInvariant(m.Value[1]));
-        
-        
+        public static string ToSentenceCase(this string @this)
+            => string.IsNullOrWhiteSpace(@this)
+                ? @this
+                : Regex.Replace(@this, "[a-z][A-Z]", m => m.Value[0] + " " + char.ToLowerInvariant(m.Value[1]));
+
+
         public static bool IsValidEmailAddress(this string emailAddress)
         {
             if (emailAddress.IsNullOrWhiteSpace()) return false;
-            return  Regex.IsMatch(emailAddress,
+            return Regex.IsMatch(emailAddress,
                 @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
                 RegexOptions.IgnoreCase);
         }
-        
-        
+
+
+        // Converts a string to sentence case.
+        public static string CapitalizeFirstCharacterRestLowerCase(this string text) =>
+            text.ToUpper()[0] + text.ToLower()[1..];
     }
-    
-    
 }
